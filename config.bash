645d44fea950a05dd559307b7bf3357dbcbfba90
Mukil Saravanan <senjerimalaimukil@gmail.com>
2022-10-29 12:12:38 +0530

    Add files via upload

---
+++ config.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Insall Vim Vundle 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
